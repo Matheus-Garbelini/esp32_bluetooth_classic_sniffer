32c73909343f842fdf591d09aedc77fcc4fc7c31
Matheus Eduardo Garbelini <mgarbelix@gmail.com>
2021-09-10 06:21:48 +0800

    simplify requirements

--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,6 @@ sudo apt install cmake clang zstd software-properties-common -y
 echo -e "\n\nInstalling latest stable wireshark"
 sudo add-apt-repository --y ppa:wireshark-dev/stable
 sudo apt-get update
-sudo apt install wireshark-dev -y
+sudo apt install wireshark wireshark-dev -y
 
 echo "Done! Run build.sh to build project"
